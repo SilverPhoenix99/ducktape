4ed6af73bb82d172328f59866d6e56b3ba32fead
Silver Phoenix <silver.phoenix99@gmail.com>
2012-04-28 16:32:13 +0100

    First version.
    Signed-off-by: Silver Phoenix <silver.phoenix99@gmail.com>

---
+++ lib/ducktape.rb
@@ -0,0 +1,9 @@
+module Ducktape
+  {
+    :Bindable                  => 'bindable',
+    :BindableAttribute         => 'bindable_attribute',
+    :BindableAttributeMetadata => 'bindable_attribute_metadata',
+    :BindingSource             => 'binding_source',
+    :Hookable                  => 'hookable'
+  }.each { |k, v| autoload k, "bindables/#{v}" }
+end
