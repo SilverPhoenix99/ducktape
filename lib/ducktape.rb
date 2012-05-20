6dedd4a9ac92d52639ced878b6705d993fbdbb95
SilverPhoenix99 <silver.phoenix99@gmail.com>
2012-05-20 12:21:27 +0100

    Bug correction: HookableCollection wasn't finding 'def_hook' method. Conventional autoloading.

--- lib/ducktape.rb
+++ lib/ducktape.rb
@@ -1,14 +1,18 @@
-require 'ducktape/version'
+require 'version'
 
 module Ducktape
   camelize = ->(f){ f.gsub(/(^|_)([^_]+)/) { |_| $2.capitalize } }
 
-  %w'bindable
-     bindable_attribute
-     bindable_attribute_metadata
-     binding_source
-     hookable
-     hookable_array
-     hookable_collection_base
-     hookable_hash'.each { |f| autoload camelize.(f), "ducktape/#{f}" }
+  Dir["#{File.expand_path('../ducktape', __FILE__)}/*.rb"].
+    map  { |f| File.basename(f, File.extname(f)) }.
+    each { |f| autoload camelize.(f), "ducktape/#{f}" }
+
+  #%w'bindable
+  #   bindable_attribute
+  #   bindable_attribute_metadata
+  #   binding_source
+  #   hookable
+  #   hookable_array
+  #   hookable_collection
+  #   hookable_hash'.each { |f| autoload camelize.(f), "ducktape/#{f}" }
 end
